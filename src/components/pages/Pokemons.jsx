58805811e68bed7e11f7d8d851a259a60392cd5d
rafiqwe <rmlmanik502@gmail.com>
2025-04-24 19:57:39 +0600

    deleted unnecessary component

--- src/components/pages/Pokemons.jsx
+++ src/components/pages/Pokemons.jsx
@@ -1,23 +1,34 @@
 import { FaSearch } from "react-icons/fa";
-import { PokemonCard } from "../LayOuts/UI/PokemonCard";
+import { useLoaderData } from "react-router-dom";
+import { PokemonCards } from "../LayOuts/UI/PokemonCards";
 export const Pokemons = () => {
-    
-    return(
-        <>
-            <section className="top_section ">
-                <div>
-                    <img src="/hero_img.png" alt="pokedex"/>
-                </div>
-                <div className="search-box">
-                    <form className="flex">
-                        <div className="input">
-                            <input type="text" placeholder="search etg, ditto or pikachu..." />
-                        </div>
-                        <button className="search"><FaSearch/></button>
-                    </form>
-                </div>
-            </section>
-            <PokemonCard/>
-        </>
-    )
-}
+  const data = useLoaderData();
+  
+  return (
+    <>
+      <section className="top_section ">
+        <div>
+          <img src="/hero_img.png" alt="pokedex" />
+        </div>
+        <div className="search-box">
+          <form className="flex">
+            <div className="input">
+              <input
+                type="text"
+                placeholder="search etg, ditto or pikachu..."
+              />
+            </div>
+            <button className="search">
+              <FaSearch />
+            </button>
+          </form>
+        </div>
+      </section>
+      <ul className="card_section">
+        {data.map((pokemon) => {
+          return <PokemonCards pokemon={pokemon} key={pokemon.id} />;
+        })}
+      </ul>
+    </>
+  );
+};
